9d24cab247fee3e3f041e80adff9ba7babdcd6c2
Ramses-Moreno <ram.ramses21sentinela@gmail.com>
2021-02-07 21:19:33 -0600

    Eprendiendo JavaScript

---
+++ JavaScript/Variables.js
@@ -0,0 +1,22 @@
+//Guardamos una cadena de caracteres
+var nombre = 'Ramses';
+var apellido = 'Moreno';
+
+//Guardamos variables numericas
+//Se declarn con la palabra var
+var numero1 = 5;
+var numero2 = 20;
+
+//Podemos guardar resultados dentro de variables
+var resultado = numero1 + numero2;
+alert(resultado);
+
+alert(nombre);
+alert(numero1 + numero2);
+
+//Nos muestre l valor de la variable
+document.write(numero1);
+document.write(nombre + ' ' + apellido);
+
+//Nos permite escribir en pantalla a atraves de la consola
+console.log('Hola consola');
